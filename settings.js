ff06e58d566c6e62ac94a6eb0431f2fd474f4488
Abrahan987 <168798723+Abrahan987@users.noreply.github.com>
2026-03-11 19:26:15 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-global.owner = ['584147226479'] // ABRAHAN-M
+global.owner = ['573229061072'] // ABRAHAN-M
 global.sessionName = 'Sessions/Owner'
 global.version = '^3.0.0'
 global.pairing_code = true
@@ -14,16 +14,16 @@ global.api = {
 
 global.bot = {
   api: 'https://api.stellarwa.xyz',
-  web: 'https://studio.diamondbots.xyz'
+  web: 'spacny.wuaze.com'
 }
 
 global.prefa = '/'
 
 global.mods = [
-  '5492916450307', // Tu
-  '5351524614', // Yo
-  '584147226479',// El
-  '5218711426787'// when :v but :'v
+  '', // Tu
+  '573229061072', // Yo
+  '',// El
+  ''// when :v but :'v
 ]
 
 global.msgglobal = 'Hubo un error, por favor reportalo.'
